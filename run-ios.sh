2b0cd1b8f70051cc6dc165c13d7b538dec14412f
Adrian Ibañez <hello@adrianibanez.info>
2025-03-08 15:27:03 +0100

    ios tests

--- run-ios.sh
+++ run-ios.sh
@@ -1,1 +1,9 @@
-OPENSSL_HASH=002a2d6b30b58bf4bea46c43bdd96365aaf8daa6c428782aa4feee06da197df3 OPENSSL_VERSION=3.4.1 mix package.ios.runtime
+OPENSSL_HASH=002a2d6b30b58bf4bea46c43bdd96365aaf8daa6c428782aa4feee06da197df3
+OPENSSL_VERSION=3.4.1
+OTP_TAG=OTP-27.2.4
+KERL_CONFIGURATION_OPTIONS="--disable-year2038"
+# ["ios", "ios-arm64", "iossimulator-x86_64", "iossimulator-arm64"]
+#--nifs "openssl,rustler_btleplug"
+mix package.ios.runtime --arch iossimulator-x86_64
+
+#mix package.ios.nif iossimulator-x86_64 rustler_btleplug
