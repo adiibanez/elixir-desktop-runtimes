4efe74a3db8ee24bab7e961e9f1358114a7cd81e
Adrian Ibañez <hello@adrianibanez.info>
2025-03-09 04:56:54 +0100

    archive liberlang.a too

--- run-ios.sh
+++ run-ios.sh
@@ -2,8 +2,14 @@ OPENSSL_HASH=002a2d6b30b58bf4bea46c43bdd96365aaf8daa6c428782aa4feee06da197df3
 OPENSSL_VERSION=3.4.1
 OTP_TAG=OTP-27.2.4
 KERL_CONFIGURATION_OPTIONS="--disable-year2038"
+
+export RUSTLER_BTLEPLUG_BUILD=1
+export WASMEX_BUILD=1
+export RUST_TARGET=ios-arm64
+#x86_64-apple-ios
 # ["ios", "ios-arm64", "iossimulator-x86_64", "iossimulator-arm64"]
 #--nifs "openssl,rustler_btleplug"
-mix package.ios.runtime --arch iossimulator-x86_64
+mix package.ios.runtime 
+#--arch ios-arm64
 
 #mix package.ios.nif iossimulator-x86_64 rustler_btleplug
