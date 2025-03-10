af212bf0c97be3f136db56ece7dd98ce868989a1
Adrian Ibañez <hello@adrianibanez.info>
2025-03-10 22:24:09 +0100

    fixing openssl build dependency

--- scripts/install_openssl_new.sh
+++ scripts/install_openssl_new.sh
@@ -1,51 +1,33 @@
 #!/bin/bash
 
-if [ -z "$OPENSSL_VERSION" ]; then
-export VSN=1.1.1v
-export VSN_HASH=d6697e2871e77238460402e9362d47d18382b15ef9f246aba6c7bd780d38a6b0
-else
-export VSN=$OPENSSL_VERSION
-export VSN_HASH=$OPENSSL_HASH
-fi
-
 if [ -z "$OPENSSL_PREFIX" ]; then
-export PREFIX=/usr/local/openssl
-else
-export PREFIX=$OPENSSL_PREFIX
+export OPENSSL_PREFIX=/usr/local/openssl
 fi 
 
-echo "OPENSSL PREFIX: $PREFIX"
-
-if [ -z "$ARCH" ]; then
-export BUILD_DIR=_build
-#export BASE_DIR=..
-else
-export BASE_ARCH=$(echo "$ARCH" | sed 's/ -D.*//') 
-echo "BASE_ARCH: $BASE_ARCH"
-export BUILD_DIR=_build/$BASE_ARCH
-#export BASE_DIR=../..
-fi
+echo "OPENSSL PREFIX: $OPENSSL_PREFIX"
 
 # install openssl
 echo "Build and install openssl......"
-
 export BASE_DIR="$(pwd)"
 
-echo "PREFIX: $PREFIX"
+echo "PREFIX: $OPENSSL_PREFIX"
 echo "BUILD_DIR: $BUILD_DIR"
 echo "BASE_DIR: $BASE_DIR"
 echo "ARCH: $ARCH"
 
-cd $BUILD_DIR
-
-cp $BASE_DIR/patch/openssl-ios.conf openssl-$VSN/Configurations/15-ios.conf && \
-cd openssl-$VSN
+cd openssl-$OPENSSL_VERSION
 
 echo `pwd`
-ls -lah 
+# ls -lah 
 
-echo ./Configure $ARCH --prefix=$PREFIX "$@"
-./Configure $ARCH --prefix=$PREFIX "$@"
+echo ./Configure $ARCH --prefix=$OPENSSL_PREFIX 
+#"$@"
+
+./Configure $ARCH --prefix=$OPENSSL_PREFIX 
+#"$@"
 make clean && make depend && make && make install_sw install_ssldirs
 
-echo "OPENSSL INSTALLED $PREFIX"
+echo "OPENSSL INSTALLED $OPENSSL_PREFIX"
+ls -lah "$OPENSSL_PREFIX/lib"
+
+#file $OPENSSL_PREFIX/lib/crypto.a
