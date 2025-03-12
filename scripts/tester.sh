c682e8c4c3503a059640a899c40fc916edcb5794
Adrian Ibañez <hello@adrianibanez.info>
2025-03-12 07:58:00 +0100

    changes

--- scripts/tester.sh
+++ scripts/tester.sh
@@ -1,10 +1,6 @@
 #LIB_ARM=/Users/adrianibanez/Documents/projects/2024_sensor-platform/checkouts/elixir-desktop-ios-example-app/liberlang.xcframework/ios-arm64/liberlang.a
 #LIB_X86=/Users/adrianibanez/Documents/projects/2024_sensor-platform/checkouts/elixir-desktop-ios-example-app/liberlang.xcframework/ios-x86_64-simulator/liberlang.a
 
-
-
-
-
 if [[ ! -z $LIB_ARM ]]; then
 nm -gU $LIB_ARM | grep -E "SSL_|crypto|erl_start"
 xcrun --sdk iphoneos clang -o test_erl_start test_erl_start.c \
